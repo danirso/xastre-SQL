35c835d28ac17f86946abb1319f9da797386a17a
danirso <111575546+danirso@users.noreply.github.com>
2022-09-12 20:22:51 -0300

    Update tudojunto.sql

--- tudojunto.sql
+++ tudojunto.sql
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS public.professor
 (
     nome character varying(255) COLLATE pg_catalog."default",
     id_materia integer,
-    "RP" integer
+    RP integer
 )
 
 TABLESPACE pg_default;
@@ -63,14 +63,14 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.professor
     OWNER to postgres;
 
-insert into professor(nome,id_materia,"rp")
+insert into professor(nome,id_materia,rp)
 VALUES ('Pannain',2897,1);
 
-INSERT into professor(nome,id_materia,"rp")
+INSERT into professor(nome,id_materia,rp)
 VALUES ('miro',3062,15000624);
 
-insert into professor(nome,id_materia,"rp")
+insert into professor(nome,id_materia,rp)
 VALUES ('xastre',3426,19007695);
 
-insert into professor(nome,id_materia,"rp")
+insert into professor(nome,id_materia,rp)
 values ('dimas',4569,16000897);
