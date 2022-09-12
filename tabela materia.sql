eb08e0b332658081197101a678ddc26059df32b3
danirso <111575546+danirso@users.noreply.github.com>
2022-09-12 10:26:07 -0300

    Add files via upload

---
+++ tabela materia.sql
@@ -0,0 +1,28 @@
+-- Table: public.materia
+
+-- DROP TABLE IF EXISTS public.materia;
+
+CREATE TABLE IF NOT EXISTS public.materia
+(
+    rp integer,
+    id integer,
+    nome character varying (255) COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.materia
+    OWNER to postgres;
+
+insert into materia (rp,id,nome)
+VALUES (15000624,3062,'calculo');
+
+insert into materia(rp,id,nome)
+VALUES (19007695,3426,'PI:desenvolvimento web');
+
+insert into materia(rp,id,nome)
+VALUES (1,2897,'organização de sistemas');
+
+INSERT into materias(rp,id,nome)
+VALUES (16000897,4569,'robotica');
+
